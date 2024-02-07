2e430ec225ee7df0da653615f5ab0cef706901a9
Mohammed Nassi <mnassi@e2r7p6.1337.ma>
2024-02-07 17:37:05 +0100

    all done

--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -4,10 +4,10 @@
 
 wp core download --allow-root
 
-wp config create --dbname=$DB_NAME --dbuser=$MY_SQL_USER --dbpass=$MY_SQL_PASS --dbhost=mariadb --allow-root
+wp config create --dbname=$DB_NAME --dbuser=$MY_SQL_USER --dbpass=$MY_SQL_PASS --dbhost=$DB_HOST --allow-root
 
-wp core install --url=$URL --title=localhost --admin_user=$MY_SQL_USER --admin_password=$MY_SQL_PASS --admin_email=medaitswa@gmail.com --allow-root
+wp core install --url=$URL --title=$URL --admin_user=$MY_SQL_USER --admin_password=$MY_SQL_PASS --admin_email=$ADMIN_EMAIL --allow-root
 
-wp user create $MY_USER_NAME sngp567@gmail.com --role=author --user_pass=$MY_USER_PASS --allow-root
+wp user create $MY_USER_NAME $EMAIL --role=$ROLE --user_pass=$MY_USER_PASS --allow-root
 
 /usr/sbin/php-fpm7.4 -F
